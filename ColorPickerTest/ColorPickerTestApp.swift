fc64203bb6fe99ca9b3de29cb1ec1e8db043df3e
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-24 15:49:07 +1100

    Initial Commit

---
+++ ColorPickerTest/ColorPickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerTestApp.swift
+//  ColorPickerTest
+//
+//  Created by Jody Clelland on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorPickerTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorPickerTest
+//
+//  Created by Jody Clelland on 24/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
